5056d46a60b80a2a9ae92cc3bd4e514dca39c4db
Steve Cobb <scobb@alumni.lsu.edu>
2015-02-04 22:36:39 -0600

    Added something that I think will make things fair. Still need to check.

--- src/FairReadWriteLock.java
+++ src/FairReadWriteLock.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.Semaphore;
+
 
 public class FairReadWriteLock {
 	// REQUIREMENTS:
@@ -11,23 +13,71 @@ public class FairReadWriteLock {
 	//     released the lock or no preceding writer thread exists
 	
 	// TODO - write tests.
-	// TODO - this is not fair
-	private int numReaders;
+	private int schedule;
+	private int turn;
+	
 	private int numWriters;
+	private int numReaders;
+	
+	private Semaphore sem;
+	
 	public FairReadWriteLock(){
-		int numReaders = 0;
-		int numWriters = 0;
+		// implement a "take a number" methodology?
+		schedule = 0;
+		turn = 0;
+		
+		numWriters = 0;
+		numReaders = 0;
+		
+		// are we allowed to use a semaphore?
+		sem = new Semaphore(1);
+		
 	}
+	private int getScheduledTurn(){
+		// helper method - acquires the semaphore to alter the shared variable schedule
+		try {
+			sem.acquire();
+		} catch (InterruptedException exc){
+			System.out.println("Exception: " + exc);
+		}
+		int result = schedule;
+		schedule++;
+		sem.release();
+		return result;
+	}
+	
+	private void incrementTurn(){
+		// helper method - acquires the semaphore to alter the shared variable turn
+		try {
+			sem.acquire();
+		} catch (InterruptedException exc){
+			System.out.println("Exception: " + exc);
+		}
+		turn++;
+		sem.release();
+	}
+	
 	void beginRead(){
-		while (numWriters > 0){
+		// Take a number.
+		int myScheduledTurn = getScheduledTurn();
+		
+		// ensure there are no writers and it's my turn to read
+		while (numWriters > 0 || myScheduledTurn < turn){
 			try {
 				wait();
 			} catch (InterruptedException exc){
 				System.out.println("Exception: " + exc);
 			}
 		}
+		
+		// once we get in, increment number of readers
 		numReaders++;
 		
+		// when I'm in, it's the next guy's turn.
+		incrementTurn();
+		
+		// because multiple people can read, we need to notify here to let other readers in.
+		notifyAll();
 	}
 	void endRead(){
 		numReaders--;
@@ -35,13 +85,21 @@ public class FairReadWriteLock {
 		
 	}
 	void beginWrite(){
-		while (numReaders > 0 || numWriters > 0){
+		
+		// take a number
+		int myScheduledTurn = getScheduledTurn();
+		
+		// if there's a reader, a writer, or it's not my turn, wait.
+		while (numReaders > 0 || numWriters > 0 || myScheduledTurn < turn){
 			try {
 				wait();
 			} catch (InterruptedException exc){
 				System.out.println("Exception: " + exc);
 			}
 		}
+		
+		// increment the turn.
+		incrementTurn();
 		
 	}
 	void endWrite(){
