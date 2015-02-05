6d56b7b8d429452a63b256d54407cf82e235fe6b
Steve Cobb <scobb@alumni.lsu.edu>
2015-02-04 21:00:42 -0600

    Initial commit.

---
+++ src/Garden.java
@@ -0,0 +1,30 @@
+
+public class Garden {
+	// implement the following function
+	// don't worry about exceptions
+	
+	// the constructor takes the MAX argument
+	public Garden(int MAX){
+		
+	}
+	
+	public void startDigging(){
+		
+	}
+	public void doneDigging(){
+		
+	}
+	public void startSeeding(){
+		
+	}
+	public void doneSeeding(){
+		
+	}
+	public void startFilling(){
+		
+	}
+	public void doneFilling(){
+		
+	}
+
+}
