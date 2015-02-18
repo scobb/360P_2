bdb82ada986df7c827445599d7c6f18a97a0d354
Steve Cobb <scobb@alumni.lsu.edu>
2015-02-18 16:45:34 -0600

    Using atomic integers instead of ints

--- src/Garden.java
+++ src/Garden.java
@@ -1,6 +1,6 @@
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
-
 
 public class Garden implements GardenCounts {
 	// implement the following function
@@ -9,134 +9,138 @@ public class Garden implements GardenCounts {
 	final private Condition readyToSeed = lock.newCondition();
 	final private Condition readyToFill = lock.newCondition();
 	final private Condition readyToDig = lock.newCondition();
-	
-	final private ReentrantLock shovel = new ReentrantLock();
-	
+
 	private int numUnseededHoles = 0;
 	private int numSeededHoles = 0;
 	private int max_holes;
 	private boolean shovelAvailable = true;
-	private int totalHolesDug = 0;
-	private int totalHolesSeeded = 0;
-	private int totalHolesFilled = 0;
-	
-	
+	private AtomicInteger totalHolesDug = new AtomicInteger(0);
+	private AtomicInteger totalHolesSeeded = new AtomicInteger(0);
+	private AtomicInteger totalHolesFilled = new AtomicInteger(0);
+
 	// the constructor takes the MAX argument
-	public Garden(int MAX){
+	public Garden(int MAX) {
 		max_holes = MAX;
 	}
-    public int totalHolesDugByNewton() {
-    	return totalHolesDug;
-    }
 
-    public int totalHolesSeededByBenjamin() {
-    	return totalHolesSeeded;
-    }
+	public int totalHolesDugByNewton() {
+		return totalHolesDug.get();
+	}
 
-    public int totalHolesFilledByMary() {
-    	return totalHolesFilled;
-    }
+	public int totalHolesSeededByBenjamin() {
+		return totalHolesSeeded.get();
+	}
+
+	public int totalHolesFilledByMary() {
+		return totalHolesFilled.get();
+	}
+
 	public void startDigging() {
 		try {
 			lock.lock();
-			//System.out.println("Trying to start digging.");
+			// System.out.println("Trying to start digging.");
 			int totalHoles = numUnseededHoles + numSeededHoles;
 			while (totalHoles == max_holes || !shovelAvailable) {
 				try {
-				readyToDig.await();
-				} catch (InterruptedException exc){}
+					readyToDig.await();
+				} catch (InterruptedException exc) {
+				}
 				totalHoles = numUnseededHoles + numSeededHoles;
-				//System.out.println("I was signalled.");
-				
+				// System.out.println("I was signalled.");
+
 			}
-				
-			//System.out.println("Starting digging.");
+
+			// System.out.println("Starting digging.");
 			shovelAvailable = false;
 		} finally {
 			lock.unlock();
 		}
-		
-		
+
 	}
-	public void doneDigging(){
+
+	public void doneDigging() {
 		try {
-			//System.out.println("Trying to acquire lock.");
+			// System.out.println("Trying to acquire lock.");
 			lock.lock();
-			//System.out.println("Lock acquired.");
+			// System.out.println("Lock acquired.");
 			++numUnseededHoles;
-			++totalHolesDug;
+			totalHolesDug.getAndIncrement();
 			shovelAvailable = true;
-			
-			
+
 			// signal for hole
-			//System.out.println("signaling readyToSeed.");
+			// System.out.println("signaling readyToSeed.");
 			readyToSeed.signal();
 			// signal for shovel
-			//System.out.println("signaling readyToFill.");
+			// System.out.println("signaling readyToFill.");
 			readyToFill.signal();
-			//System.out.println("Done digging.");
+			// System.out.println("Done digging.");
 		} finally {
 			lock.unlock();
 		}
-		
+
 	}
+
 	public void startSeeding() {
 		try {
-			//System.out.println("Trying to start seeding.");
+			// System.out.println("Trying to start seeding.");
 			lock.lock();
-			while (numUnseededHoles == 0) { 
+			while (numUnseededHoles == 0) {
 				try {
-				readyToSeed.await();
-				} catch (InterruptedException exc){}
+					readyToSeed.await();
+				} catch (InterruptedException exc) {
+				}
 			}
-			//System.out.println("Starting seeding.");
+			// System.out.println("Starting seeding.");
 		} finally {
 			lock.unlock();
 		}
 	}
-	public void doneSeeding(){
+
+	public void doneSeeding() {
 		try {
 			lock.lock();
-			++totalHolesSeeded;
+			totalHolesSeeded.getAndIncrement();
 			++numSeededHoles;
 			--numUnseededHoles;
 			readyToFill.signal();
-			//System.out.println("Done seeding.");
+			// System.out.println("Done seeding.");
 		} finally {
 			lock.unlock();
 		}
-		
+
 	}
+
 	public void startFilling() {
 		try {
 			lock.lock();
-			//System.out.println("Trying to start filling.");
-			while (numSeededHoles == 0 || ! shovelAvailable)  {
+			// System.out.println("Trying to start filling.");
+			while (numSeededHoles == 0 || !shovelAvailable) {
 				try {
-				readyToFill.await();
-				} catch (InterruptedException exc){}
-				
+					readyToFill.await();
+				} catch (InterruptedException exc) {
+				}
+
 			}
 			shovelAvailable = false;
-			//System.out.println("Filling started.");
+			// System.out.println("Filling started.");
 		} finally {
 			lock.unlock();
 		}
-		
+
 	}
-	public void doneFilling(){
+
+	public void doneFilling() {
 		try {
 			lock.lock();
-			++totalHolesFilled;
+			totalHolesFilled.getAndIncrement();
 			--numSeededHoles;
 			shovelAvailable = true;
 			readyToDig.signal();
-			//System.out.println("Filling done.");
+			// System.out.println("Filling done.");
 		} finally {
 			lock.unlock();
 		}
-		
-		
+
 	}
 
 }
